2ab942c315c6925b1da70737fa782659bae6a115
Aditya Raj singh <9555adityarajsingh@gmail.com>
2025-09-27 01:07:42 +0530

    update tailwind

---
+++ TAILWIND/src/App.js
@@ -0,0 +1,132 @@
+import React , {useState} from "react"; 
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return(
+        <div className="flex flex-wrap justify-center gap-6 p-5 bg-gray-100 min-h-screen">
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+        <div className="max-w-sm overflow-hidden bg-white shadow-md p-4 mt-5">
+            <div>
+                <img  className="w-full h-48 object-cover rounded-2xl " src="https://cdn.dnaindia.com/sites/default/files/2023/05/09/2588863-untitled-design-2023-05-09t173334.095.jpg?im=FitAndFill=(1200,900)"></img>
+            </div>
+            <div className="mt-4 font-bold text-2xl text-gray-700">
+                <h1>About Me</h1>
+            </div>
+            <div className="text-gray-500 mt-2">
+                <p>Rohit Negi is a good boy</p>
+            </div>
+            <div className="mt-3 flex justify-center">
+                <button className="bg-amber-500 p-1 rounded-xl text-white text-2xl hover:bg-amber-800">Know more</button>
+            </div>
+        </div>
+
+    </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App></App>);
